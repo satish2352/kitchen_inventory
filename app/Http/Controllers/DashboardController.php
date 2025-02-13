a8cc09e4a057c94275c1b17b61cf51b27ddd815b
mhaskemahesh <maheshmhaske500@gmail.com>
2025-02-13 16:58:19 +0530

    dashboard count code added

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,7 +13,9 @@ use App\Models\ {
     Unit,
     User,
     UsersData,
-    MasterKitchenInventory
+    MasterKitchenInventory,
+    ActivityLog,
+    LocationWiseInventory
 };
 use Session;
 use Cookie;
@@ -27,19 +29,127 @@ class DashboardController extends Controller
 
     public function index(Request $request) 
     {
-        $userCount = UsersData::where('is_deleted', '0')->count();
-        $MasterInventoryCount = MasterKitchenInventory::where('is_deleted', '0')->count();
-        // dd($userCount);
-                                // $userCount = UsersData::where('role_id', 2)
-                                // ->orWhere('role_id', 3)
-                                // ->count();
+        $sess_user_id = session()->get('login_id');
+        $location_selected_name = session()->get('location_selected_name');
+        $location_selected_id = session()->get('location_selected_id');
+
+        
+        if($sess_user_id =='22'){
+        $userCount = UsersData::where('is_deleted', '0') -> where('is_approved', '1') ->count();
+
+        // ---------------------------
+        $userLocationData = UsersData::where('is_deleted', '0')
+        ->where('is_approved', '1')
+        ->where('id', $sess_user_id)
+        ->pluck('location')
+        ->toArray(); 
+        $userLocation = [];
+        foreach ($userLocationData as $location) {
+            $userLocation = array_merge($userLocation, explode(',', $location));
+        }
+        $MasterInventoryCount = MasterKitchenInventory::where('is_deleted', '0')
+        ->whereIn('location_id', $userLocation)
+        ->groupBy('location_id')
+        ->count();
+        // ---------------------------
+
+        $ActivityLogCount = ActivityLog::count();
+        $Count = LocationWiseInventory::selectRaw('DATE(created_at) as date, COUNT(*) as count')
+        ->where('is_deleted', '0')
+        ->groupBy('date')
+        ->get();
+
+        $LocationWiseInventoryCount = $Count->count();
+
         $return_data = [
             'status' => 'true',
             'message' => 'Counts retrieved successfully',
             'users_count' => $userCount,
-            'master_inventory_count' => $MasterInventoryCount
+            'master_inventory_count' => $MasterInventoryCount,
+            'ActivityLogCount' => $ActivityLogCount,
+            'LocationWiseInventoryCount' => $LocationWiseInventoryCount,
+            'sess_user_id' => $sess_user_id
+
         ];
         return view('dashboard',compact('return_data'));
+
+        }else if($sess_user_id =='14'){
+
+        $userCount = UsersData::where('is_deleted', '0')
+        ->where('added_by', '2')
+        ->where('added_byId', $sess_user_id)
+        ->count();
+
+        $alluserCount = UsersData::where('is_deleted', '0')
+        ->count();
+    
+        // ----------------------------
+
+        $userLocationData = UsersData::where('is_deleted', '0')
+        ->where('is_approved', '1')
+        ->where('id', $sess_user_id)
+        ->pluck('location')
+        ->toArray(); 
+        $userLocation = [];
+        foreach ($userLocationData as $location) {
+            $userLocation = array_merge($userLocation, explode(',', $location));
+        }
+
+        $LocationWiseInventoryCount = LocationWiseInventory::where('is_deleted', '0')
+        ->whereIn('location_id', $userLocation)
+        ->groupBy('location_id')
+        ->count();
+
+        $return_data = [
+            'status' => 'true',
+            'message' => 'Counts retrieved successfully',
+            'users_count' => $userCount,
+            'LocationWiseInventoryCount' => $LocationWiseInventoryCount,
+            'sess_user_id' => $sess_user_id,
+            'alluserCount' => $alluserCount
+
+        ];
+        return view('dashboard',compact('return_data'));
+    }else if($sess_user_id =='12'){
+
+        $userCount = UsersData::where('is_deleted', '0')
+        ->where('added_by', '2')
+        ->where('added_byId', $sess_user_id)
+        ->count();
+        // dd($userCount);
+
+        $alluserCount = UsersData::where('is_deleted', '0')
+        ->count();
+    
+        // ----------------------------
+
+        $userLocationData = UsersData::where('is_deleted', '0')
+        ->where('is_approved', '1')
+        ->where('id', $sess_user_id)
+        ->pluck('location')
+        ->toArray(); 
+        $userLocation = [];
+        foreach ($userLocationData as $location) {
+            $userLocation = array_merge($userLocation, explode(',', $location));
+        }
+
+        $LocationWiseInventoryCount = LocationWiseInventory::where('is_deleted', '0')
+        ->whereIn('location_id', $userLocation)
+        ->groupBy('location_id')
+        ->count();
+
+        $return_data = [
+            'status' => 'true',
+            'message' => 'Counts retrieved successfully',
+            'users_count' => $userCount,
+            'LocationWiseInventoryCount' => $LocationWiseInventoryCount,
+            'sess_user_id' => $sess_user_id,
+            'alluserCount' => $alluserCount
+
+        ];
+        return view('dashboard',compact('return_data'));
+        }
+        
     }
 
     public function logout(Request $request)
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -28,62 +28,85 @@
     </div>
  </div>
  <!-- box content -->
+
+ <!-- <img src="{{ asset('/img/main_logo.png') }}" alt="Company Logo dsds"> -->
+
+ <!-- <img src="{{ asset('/img/main_logo.png') }}" alt="Company Logo" /> -->
+ @if ($return_data['sess_user_id'] =='22')
  <div class="service-box jost-font">
     <div class="container-fluid p-3">
        <div class="row d-flex g-2">
           <!-- Icon Box 1 -->
+          @if ($return_data['users_count'] > 0)
           <div class="col-4">
-             <div class="icon-box text-center shadow">
-                <div class="icon-circle mb-3">
-                   <i class="bi bi-person-add"></i>
-                </div>
-                <div class="icon-divider"></div>
-                <span class="mt-3">Approve Users</span>
-             </div>
-          </div>
+            <div class="icon-box text-center shadow">
+               <div class="icon-circle mb-3">
+                     <i class="bi bi-person-add"></i>
+               </div>
+               <h4 class="mt-2">{{ $return_data['users_count'] }}</h4> <!-- Moved below the icon -->
+               <div class="icon-divider"></div>
+               <span class="mt-3">Approve Users</span>
+            </div>
+         </div>
+          @endif
           <!-- Icon Box 2 -->
+
+
+          @if ($return_data['ActivityLogCount'] > 0)
           <div class="col-4">
-             <div class="icon-box text-center shadow">
-                <div class="icon-circle mb-3">
-                   <i class="bi bi-person-add"></i>
-                </div>
-                
-                <div class="icon-divider"></div>
-                <span class="mt-3">Activity</span>
-             </div>
-          </div>
+            <div class="icon-box text-center shadow">
+               <div class="icon-circle mb-3">
+                     <i class="bi bi-person-add"></i>
+               </div>
+               <h4 class="mt-2">{{ $return_data['ActivityLogCount'] }}</h4> <!-- Moved below the icon -->
+               <div class="icon-divider"></div>
+               <span class="mt-3">Activity</span>
+            </div>
+         </div>
+          @endif
+
           <!-- Icon Box 3 -->
+          @if ($return_data['LocationWiseInventoryCount'] > 0)
           <div class="col-4">
-             <div class="icon-box text-center shadow">
-                <div class="icon-circle mb-3">
-                   <i class="bi bi-share-fill"></i>
-                </div>
-                <div class="icon-divider"></div>
-                <span class="mt-3">Shopping List</span>
-             </div>
-          </div>
+            <div class="icon-box text-center shadow">
+               <div class="icon-circle mb-3">
+                     <i class="bi bi-share-fill"></i>
+               </div>
+               <h4 class="mt-2">{{ $return_data['LocationWiseInventoryCount'] }}</h4> <!-- Moved below the icon -->
+               <div class="icon-divider"></div>
+               <span class="mt-3">Shopping List</span>
+            </div>
+         </div>
+          @endif
+
           <!-- Icon Box 4 -->
+          @if ($return_data['LocationWiseInventoryCount'] > 0)
           <div class="col-4">
              <div class="icon-box text-center shadow">
                 <div class="icon-circle mb-3">
                    <i class="bi bi-tools"></i>
                 </div>
+               <h4 class="mt-2">{{ $return_data['LocationWiseInventoryCount'] }}</h4> <!-- Moved below the icon -->
                 <div class="icon-divider"></div>
                 <span class="mt-3">kitchen Inventory</span>
              </div>
           </div>
+          @endif
           <!-- Icon Box 5 -->
+          @if ($return_data['master_inventory_count'] > 0)
           <div class="col-4">
-             <div class="icon-box text-center shadow">
-                <div class="icon-circle mb-3">
-                   <i class="bi bi-journal-bookmark-fill"></i>
-                </div>
-                <div class="icon-divider"></div>
-                <span class="mt-3">Master Inventory</span>
-             </div>
-          </div>
+            <div class="icon-box text-center shadow">
+               <div class="icon-circle mb-3">
+                     <i class="bi bi-person-add"></i>
+               </div>
+               <h4 class="mt-2">{{ $return_data['master_inventory_count'] }}</h4> <!-- Moved below the icon -->
+               <div class="icon-divider"></div>
+               <span class="mt-3">Master Inventory</span>
+            </div>
+         </div>
+          @endif
           <!-- Icon Box 6 -->
-          <div class="col-4">
+          <!-- <div class="col-4">
              <div class="icon-box text-center shadow">
                 <div class="icon-circle mb-3">
                    <i class="bi bi-cart3"></i>
@@ -91,10 +114,132 @@
                 <div class="icon-divider"></div>
                 <span class="mt-3">Submit Shopping list</span>
              </div>
-          </div>
+          </div> -->
        </div>
     </div>
  </div>
+ @elseif ($return_data['sess_user_id'] =='14')
+ <div class="service-box jost-font">
+    <div class="container-fluid p-3">
+       <div class="row d-flex g-2">
+          <!-- Icon Box 1 -->
+          @if ($return_data['alluserCount'] > 0)
+          <div class="col-4">
+            <div class="icon-box text-center shadow">
+               <div class="icon-circle mb-3">
+                     <i class="bi bi-person-add"></i>
+               </div>
+               <h4 class="mt-2">{{ $return_data['alluserCount'] }}</h4> <!-- Moved below the icon -->
+               <div class="icon-divider"></div>
+               <span class="mt-3">All Users</span>
+            </div>
+         </div>
+          @endif
+          <!-- Icon Box 2 -->
+
+          <!-- Icon Box 1 -->
+          @if ($return_data['users_count'] > 0)
+          <div class="col-4">
+            <div class="icon-box text-center shadow">
+               <div class="icon-circle mb-3">
+                     <i class="bi bi-person-add"></i>
+               </div>
+               <h4 class="mt-2">{{ $return_data['users_count'] }}</h4> <!-- Moved below the icon -->
+               <div class="icon-divider"></div>
+               <span class="mt-3">Approve Users</span>
+            </div>
+         </div>
+          @endif
+          <!-- Icon Box 2 -->
+
+          <!-- Icon Box 3 -->
+          @if ($return_data['LocationWiseInventoryCount'] > 0)
+          <div class="col-4">
+            <div class="icon-box text-center shadow">
+               <div class="icon-circle mb-3">
+                     <i class="bi bi-share-fill"></i>
+               </div>
+               <h4 class="mt-2">{{ $return_data['LocationWiseInventoryCount'] }}</h4> <!-- Moved below the icon -->
+               <div class="icon-divider"></div>
+               <span class="mt-3">Shopping List</span>
+            </div>
+         </div>
+          @endif
+
+          <!-- Icon Box 4 -->
+          @if ($return_data['LocationWiseInventoryCount'] > 0)
+          <div class="col-4">
+             <div class="icon-box text-center shadow">
+                <div class="icon-circle mb-3">
+                   <i class="bi bi-tools"></i>
+                </div>
+               <h4 class="mt-2">{{ $return_data['LocationWiseInventoryCount'] }}</h4> <!-- Moved below the icon -->
+                <div class="icon-divider"></div>
+                <span class="mt-3">kitchen Inventory</span>
+             </div>
+          </div>
+          @endif
+          
+          <!-- Icon Box 6 -->
+          <!-- <div class="col-4">
+             <div class="icon-box text-center shadow">
+                <div class="icon-circle mb-3">
+                   <i class="bi bi-cart3"></i>
+                </div>
+                <div class="icon-divider"></div>
+                <span class="mt-3">Submit Shopping list</span>
+             </div>
+          </div> -->
+       </div>
+    </div>
+ </div>
+ @elseif ($return_data['sess_user_id'] =='12')
+ <div class="service-box jost-font">
+    <div class="container-fluid p-3">
+       <div class="row d-flex g-2">
+          
+          <!-- Icon Box 3 -->
+          @if ($return_data['LocationWiseInventoryCount'] > 0)
+          <div class="col-4">
+            <div class="icon-box text-center shadow">
+               <div class="icon-circle mb-3">
+                     <i class="bi bi-share-fill"></i>
+               </div>
+               <h4 class="mt-2">{{ $return_data['LocationWiseInventoryCount'] }}</h4> <!-- Moved below the icon -->
+               <div class="icon-divider"></div>
+               <span class="mt-3">Shopping List</span>
+            </div>
+         </div>
+          @endif
+
+          <!-- Icon Box 4 -->
+          @if ($return_data['LocationWiseInventoryCount'] > 0)
+          <div class="col-4">
+             <div class="icon-box text-center shadow">
+                <div class="icon-circle mb-3">
+                   <i class="bi bi-tools"></i>
+                </div>
+               <h4 class="mt-2">{{ $return_data['LocationWiseInventoryCount'] }}</h4> <!-- Moved below the icon -->
+                <div class="icon-divider"></div>
+                <span class="mt-3">kitchen Inventory</span>
+             </div>
+          </div>
+          @endif
+          
+          <!-- Icon Box 6 -->
+          <!-- <div class="col-4">
+             <div class="icon-box text-center shadow">
+                <div class="icon-circle mb-3">
+                   <i class="bi bi-cart3"></i>
+                </div>
+                <div class="icon-divider"></div>
+                <span class="mt-3">Submit Shopping list</span>
+             </div>
+          </div> -->
+       </div>
+    </div>
+ </div>
+ @endif
  <hr />
  
  <!-- Delete Confirmation Popup -->
