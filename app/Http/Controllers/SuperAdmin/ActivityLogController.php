795e75a0cd9135bdd8df47495674168eea986063
Satish <satishaurange@sumagoinfotech.com>
2025-02-26 01:51:52 +0530

    changes for activity log

--- app/Http/Controllers/SuperAdmin/ActivityLogController.php
+++ app/Http/Controllers/SuperAdmin/ActivityLogController.php
@@ -1,53 +1,50 @@
 <?php
-
 namespace App\Http\Controllers\SuperAdmin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Validator;
-use App\Http\Services\Manager\ShoppingListServices;
-use App\Models\ {
-    Items,
-    Locations,
-    LocationWiseInventory,
-    MasterKitchenInventory,
+use App\Models\{
     ActivityLog
 };
-use Session;
-use Cookie;
 
 class ActivityLogController extends Controller
 {
     public function __construct()
     {
-        $this->service = new ShoppingListServices();
 
     }
 
-    public function getActivityLog() 
-{
-    $sess_user_id = session()->get('login_id');
-    $location_selected_name = session()->get('location_selected_name');
-    $location_selected_id = session()->get('location_selected_id');
+    public function getActivityLog()
+    {
+        try {
+            $sess_user_id = session()->get('login_id');
+            $location_selected_name = session()->get('location_selected_name');
+            $location_selected_id = session()->get('location_selected_id');
 
-    $ActiviyLogData = ActivityLog::select('id', 'activity_message', 'created_at')
-        ->orderBy('created_at', 'desc') // Optional: Sort by latest first
-        ->paginate(10); // Change 10 to the number of items per page
-// dd($ActiviyLogData);
-    return view('activity', compact('ActiviyLogData'));
-}
+            $ActiviyLogData = ActivityLog::select('id', 'activity_message', 'created_at')
+                ->orderBy('created_at', 'desc') // Optional: Sort by latest first
+                ->paginate(10); // Change 10 to the number of items per page
+            return view('activity', compact('ActiviyLogData'));
+        } catch (\Exception $e) {
+            info($e->getMessage());
+        }
+    }
 
-public function searchActivityLog(Request $request)
-{
-    $query = $request->input('query');
-    $sess_user_id = session()->get('login_id');
+    public function searchActivityLog(Request $request)
+    {
+        try {
+            $query = $request->input('query');
+            $sess_user_id = session()->get('login_id');
 
-    $ActiviyLogData = ActivityLog::select('id', 'activity_message', 'created_at')
-            ->where('activity_message', 'like', "%$query%")
-        ->orderBy('created_at', 'desc') // Optional: Sort by latest first
-        ->paginate(10);
-    return view('activity-search-results', compact('ActiviyLogData'))->render();
-}
+            $ActiviyLogData = ActivityLog::select('id', 'activity_message', 'created_at')
+                ->where('activity_message', 'like', "%$query%")
+                ->orderBy('created_at', 'desc') // Optional: Sort by latest first
+                ->paginate(10);
+            return view('activity-search-results', compact('ActiviyLogData'))->render();
+        } catch (\Exception $e) {
+            info($e->getMessage());
+        }
+    }
 
 
 }
